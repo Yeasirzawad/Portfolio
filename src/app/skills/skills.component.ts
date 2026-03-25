7ff2f8a437fd7ea00a5b8f719eab9fa0906fe781
Yeasirzawad <amirsifat@gmail.com>
2026-03-26 01:44:56 +0600

    market research skills added

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -31,9 +31,15 @@ export class SkillsComponent {
     },
     {
       category: 'Analysis',
-      skills: [
-        'Descriptive Analysis', 'Quantitative Analysis', 'Statistical Analysis',
+      skills: ['Descriptive Analysis', 'Quantitative Analysis', 'Statistical Analysis',
         'Predictive Modelling', 'Regression Analysis', 'Time Series Analysis'
+      ]
+    },
+    {
+      category: 'Market Research & Business Intelligence',
+      skills: ['TAM/SAM/SOM Estimation', 'Market Size Forecasting','Competitive Analysis', 'SWOT Analysis', 
+        'Customer Segmentation','M&A Evaluation', 'Trend Analysis', 'Sentiment Analysis', 'Business Intelligence', 
+        'Data-Driven Decision Making', 'Data Storytelling', 'Report Writing'
       ]
     }
   ];

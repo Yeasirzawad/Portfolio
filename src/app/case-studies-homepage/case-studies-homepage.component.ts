cddc1a1c6762050487e9ecb9ab1fa02d816a6d03
Yeasirzawad <amirsifat@gmail.com>
2026-06-05 20:13:45 +0600

    checking everything

--- src/app/case-studies-homepage/case-studies-homepage.component.ts
+++ src/app/case-studies-homepage/case-studies-homepage.component.ts
@@ -14,3 +14,4 @@ import { CaseStudyCardComponent } from '../case-study-card/case-study-card.compo
 export class CaseStudiesHomepageComponent {
   featuredCaseStudies: CaseStudy[] = getFeaturedCaseStudies();
 }
+dads
